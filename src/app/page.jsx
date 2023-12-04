e5f0cd31e2e695e1906c8aaae69f1c23dc2b7e52
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-12-04 19:51:49 +0530

    add body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,24 @@
-import HomeHeader from '@/components/HomeHeader'
-import React from 'react'
+import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
     <>
-      <HomeHeader/>
+      {/* header */}
+      <HomeHeader />
+
+      {/* body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width="300"
+          height="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+        />
+
+        <HomeSearch/>
+      </div>
     </>
-  )
+  );
 }
