2a31c758e9c85009646612e2d07b6d402d1b7253
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-12-06 19:09:08 +0530

    add footer component

---
+++ src/components/CountryLookUp.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React, { useState } from 'react'
+
+export default function CountryLookUp() {
+    const [country, setCountry] = useState("India")
+  return (
+    <div>
+      {country}
+    </div>
+  )
+}
