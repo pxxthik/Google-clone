02231e45330919e61d5c63b9b8159e7292896457
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-12-13 07:35:58 +0530

    handle possible errors

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default async function page({ searchParams }) {
@@ -5,8 +6,25 @@ export default async function page({ searchParams }) {
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
 
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
   const data = await response.json();
-  console.log(data);
   const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try search for something else or go back to homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
   return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
