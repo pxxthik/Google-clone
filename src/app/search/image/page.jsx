c83cc07d6fa0920f1d475cc4caab47bdbb6a24dd
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-12-14 12:19:34 +0530

    add image results

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,5 +1,31 @@
+import ImageSearchResults from "@/components/ImageSearchResults";
+import Link from "next/link";
 import React from "react";
 
-export default function page() {
-  return <div>ImageSearch</div>;
+export default async function ImageSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`
+  );
+
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  const data = await response.json();
+  const results = data.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try search for something else or go back to homepage{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+  return <>{results && <ImageSearchResults results={data}/>}</>;
 }
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -2,7 +2,7 @@ import WebSearchResults from "@/components/WebSearchResults";
 import Link from "next/link";
 import React from "react";
 
-export default async function page({ searchParams }) {
+export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
